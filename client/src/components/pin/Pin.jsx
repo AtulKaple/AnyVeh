678fb704fbd6bfa31421468c72076feaa4c8ec56
Atul Kaple <109103910+AtulKaple@users.noreply.github.com>
2024-08-06 14:05:33 +0530

    Update Pin.jsx

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -1,16 +1,22 @@
 import { Marker, Popup } from 'react-leaflet'
 import './pin.scss'
 import { Link } from 'react-router-dom'
+import { icon } from "leaflet"
+
+const ICON = icon({
+  iconUrl: "/marker.png",
+  iconSize: [32, 39],
+})
 
 function Pin({item}) {
   return (
-    <Marker position={[item.latitude,item.longitude]}>
+    <Marker icon={ICON} position={[item.latitude,item.longitude]}>
       <Popup className='main'>
        <div className="popupContainer">
         <img src={item.images[0]} alt="" />
         <div className="textContainer">
             <Link to={`/${item.id}`}>{item.title}</Link>
-            <span>{item.bedroom} bedroom</span>
+            <span>₹{item.price}</span>
             <b>${item.price}</b>
         </div>
        </div>
